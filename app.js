a83c60004089e0e7483bd937dbcce4f0fb0672fd
vinchi777 <jude_calimbas@yahoo.com>
2014-02-09 21:28:48 +0800

    working

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var path = require('path');
 
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(3000);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -33,6 +37,97 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var cheerio = require('cheerio');
+var request = require('request');
+var url = "http://www.rarejob.com/reservation/";
+var async = require('async');
+
+io.sockets.on('connection', function (socket) {
+	socket.on('use_loop',function(){
+		console.log('Starting to scrape data using loop');
+		use_loop(socket);
+	})	
+	socket.on('use_whilst',function(){
+		console.log('Starting to scrape data using whilst');
+		use_whilst(socket);
+	})
 });
+
+
+function use_loop(socket){
+	options = {
+		url: getRareJobUrl(),
+		headers: {
+			'X-Requested-With': 'XMLHttpRequest'
+		}	
+	}
+	var reservations = 0;
+
+	request(options, function(err, ajax_res, body){
+		if(!err && ajax_res.statusCode == 200){
+			for(var i = 0; i < getMaxPage(body); i ++){
+				(function(index){
+					request({url: getRareJobUrl(index + 1), headers: {'X-Requested-With': 'XMLHttpRequest'} }, function(err, response, body){
+						if(!err && response.statusCode == 200){
+							$ = cheerio.load(body);
+							reservations += $(".reserveBtn").length;
+							socket.emit('loop',{ reservations: reservations });
+						}else{
+							console.log(err);
+							index --;
+						}
+					});		
+				})(i);
+			}
+		}else{ console.log(err); }
+	});
+}
+
+function use_whilst(socket){
+	options = {
+		url: getRareJobUrl(),
+		headers: {
+			'X-Requested-With': 'XMLHttpRequest'
+		}	
+	}
+	var reservations = 0;
+	var count = 0;
+
+	request(options, function(err, ajax_res, body){
+		if(!err && ajax_res.statusCode == 200){
+			async.whilst(
+				function () {return count < getMaxPage(body)},
+				function (callback) {
+					request({url: getRareJobUrl(count + 1), headers: {'X-Requested-With': 'XMLHttpRequest'} }, function(err, response, body){
+						if(!err && response.statusCode == 200){
+							$ = cheerio.load(body);
+							reservations += $(".reserveBtn").length;
+							socket.emit('whilst',{ reservations: reservations });
+						}else{ console.log(err); }
+						count++;
+						callback();
+					});		
+				},
+				function () {}
+			);
+		}else{console.log(err);}
+	});
+}
+
+function getMaxPage(body){
+	$ = cheerio.load(body);
+	ajax_link = $('ul.pager > li:last-child a').attr('href');
+	last_page = ajax_link.split('/')[ajax_link.split('/').indexOf('page') + 1];
+	return last_page;
+}
+
+function getRareJobUrl(page){
+	var date = new Date(),
+		year = date.getFullYear(),
+		month = (date.getMonth() + 1),
+		day = date.getDate();
+	page = page || 1;
+	url = 'http://www.rarejob.com/ajax/reservationSearch/?year='+year+'&month='+month+'&day='+day+'&time1=0&time2=0&jpLevel=0&gender=0&majors=&page='+page+'&characters=';
+	return url
+}
+
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,11 @@
  * GET home page.
  */
 
-var cheerio = require('cheerio');
-var request = require('request');
-var url = "http://www.rarejob.com/reservation/";
 
 exports.index = function(req, res){
-  options = {
-    url: 'http://www.rarejob.com/ajax/reservationSearch/?year=2014&month=2&day=7&time1=0&time2=0&jpLevel=0&gender=0&majors=&page=25&characters=',
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  }
-
-  request(options, function(err, res, body){
-    if(!err && res.statusCode == 200){
-      console.log(body);
-    }else{
-      console.log(err);
-    }
-
-  });
-
-  res.render('index', { title: "Ninja Moves" });
+	res.render('index', { message: "Getting all message" });
 };
+
+
+
+
